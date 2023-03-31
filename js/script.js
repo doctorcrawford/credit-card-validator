c5d8c404938f41fc0dd8659d4558264fbc412773
Kyle Crawford <kcraw217@gmail.com>
2023-03-30 22:09:32 -0700

    apply Luhn's Algorithm to verity credit cards

--- js/script.js
+++ js/script.js
@@ -2,24 +2,26 @@
 
 function ccValidator(ccNumber) {
   const ccArray = ccNumber.toString().split("").map(Number);
-  console.log(ccArray);
-  let ccArrayDoubled = [];
 
-  for (let i = 0; i < ccArray.length; i ++) {
-    if (i%2 !== 0) {
-      ccArrayDoubled.push(ccArray[i] * 2);
-    } else {
-      ccArrayDoubled.push(ccArray[i]);
+  for (let i = ccArray.length -1; i >=0; i -= 2) {
+    ccArray[i] = ccArray[i] * 2;
+    if (ccArray[i] >9) {
+      const twoDigits = ccArray[i].toString().split("").map(Number);
+      const sum = twoDigits[0] + twoDigits[1];
+      ccArray[i] = sum;
     }
   } 
- 
-  let ccArrayDubs = [];
 
-  for (let i = 0; i < ccArray.length; i ++) {
-    if (ccArrayDoubled[i] > 9) {
-      
-    }
+  let sumTotal = 0;
+  for (let i = 0; i < ccArray.length; i++) {
+    sumTotal = sumTotal + ccArray[i];
   }
   
+  const sumArray = sumTotal.toString(). split("").map(Number);
+  if (sumArray[sumArray.length - 1] === 0) {
+    return 'This credit card is valid!';
+  } else {
+    return 'This credit card is not valid :(';
+  }
 
 }
